af6bf5f46677c556aed6da15185a825e33874065
zacsketches <zacsketches@users.noreply.github.com>
2018-08-19 16:53:29 -0500

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,6 +12,10 @@ public class Greeter {
 
   }
 
+  /**
+  *@param someone is the person to greet
+  *@return a sring greeting.
+  */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
